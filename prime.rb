5fe13e30044c07bfa18a2f38f1f240a4c8fa5de6
Kachel Wittig <kjwittig@gmail.com>
2018-03-16 22:58:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ def prime?(n)
     n = number.floor
     return true if n < 2
     max = Math.sqrt(n)
-    (2..max).none?{|k| 0 == n % k}
-
+    if (2..max).none?{|k| 0 == n % k}
+    return
+    false  
 end
