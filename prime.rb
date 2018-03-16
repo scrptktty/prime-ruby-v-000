2901cd6064b6c9f3642fc4a378a4889f05bdab2f
Kachel Wittig <kjwittig@gmail.com>
2018-03-16 23:42:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 def prime?(n)
-  while n < 2 && n > 0
-    return true
-  end
 
-  n.each do |num|
-    sqrt_int = Math.sqrt(n).floor
+  sqrt_int = Math.sqrt(n).floor
 
-    (2..sqrt_int).any? {|int| sqrt_int % int == 0 }
-      return true
-  end
+  not (2..sqrt_int).any? {|int| n % int == 0 }
+
 end
